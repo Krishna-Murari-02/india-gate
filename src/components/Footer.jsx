ec27bf298a0f01b6427196d9bb186c06d21c821c
Abhishek Chauhan <abhishekchauhan@Abhisheks-MacBook-Pro.local>
2024-08-11 01:05:58 +0530

    font fized

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="container">
-      <div className=" absolute bottom-0 left-0 w-full bg-[#682E21] opacity-80 h-16 flex items-center justify-between px-16">
+      <div className=" absolute bottom-0 left-0 w-full bg-[#682E21] bg-opacity-60 h-[75px] flex items-center justify-between px-16">
         <div className="flex justify-between text-white gap-[50px]">
-          <a href="">About Us</a>
+          <a href="" className="font-Inter text-[16px] font-normal">About Us</a>
           <a href="">Privacy Policy</a>
           <a href="">Terms and Conditions</a>
         </div>
